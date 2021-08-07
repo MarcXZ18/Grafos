92fbe00db538d9a116069f4dd6eb741c0e56d17b
Marco David Chávez Chambi <83045424+MarcXZ18@users.noreply.github.com>
2021-08-07 18:29:03 -0500

    Create Arco.java

---
+++ Arco.java
@@ -0,0 +1,25 @@
+public class Arco {
+	private Vertice origen;
+	private Vertice destino;
+	private int peso;
+	
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	public Vertice getOrigen() {
+		return origen;
+	}	
+	public void setOrigen(Vertice origen) {
+		this.origen = origen;
+	}	
+	public Vertice getDestino() {
+		return destino;
+	}	
+	public void setDestino(Vertice destino) {
+		this.destino = destino;
+	}
+	
+}
