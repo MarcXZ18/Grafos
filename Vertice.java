eff31f4cd7e302037bd3774c2bcbefa2bef61057
Marco David Chávez Chambi <83045424+MarcXZ18@users.noreply.github.com>
2021-08-07 18:30:49 -0500

    Create Vertice.java

---
+++ Vertice.java
@@ -0,0 +1,13 @@
+public class Vertice {
+	private String dato;
+	
+	public Vertice(String dato) {
+		this.dato = dato;
+	}
+	public String getDato() {
+		return dato;
+	}	
+	public void setDato(String dato) {
+		this.dato = dato;
+	}
+}
